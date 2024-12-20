99ac23ecc98f50af842d2653048d56b66b98d00f
Cade Rosche <60630493+cprosche@users.noreply.github.com>
2024-12-20 08:52:40 -0700

    parsed rest of fields (#6)

--- tle_test.go
+++ tle_test.go
@@ -7,24 +7,48 @@ import (
 )
 
 func TestTLEStringParse(t *testing.T) {
+
+	expected := TLE{
+		Name:  "ISS (ZARYA)",
+		Line1: "1 25544U 98067A   20274.51782528  .00000867  00000-0  22813-4 0  9991",
+		Line2: "2 25544  51.6441  93.0000 0001400  11.0000 349.0000 15.49300070250768",
+
+		NoradId:        25544,
+		Classification: "U",
+
+		LaunchTwoDigitYear: "98",
+		LaunchNumber:       "067",
+		LaunchPiece:        "A",
+
+		EpochYear: "20",
+		EpochDay:  "274.51782528",
+
+		MeanMotionFirstDerivative:  ".00000867",
+		MeanMotionSecondDerivative: "00000-0",
+
+		BStar: "22813-4",
+
+		EphemerisType:    "0",
+		ElementSetNumber: 999,
+		Line1Checksum:    1,
+
+		InclinationDegrees:     51.6441,
+		RightAscensionDegrees:  93.0000,
+		Eccentricity:           0.0001400,
+		PerigeeArgumentDegrees: 11.0000,
+		MeanAnomalyDegrees:     349.0000,
+		MeanMotion:             15.49300070,
+		EpochRevolutionCount:   25076,
+
+		Line2Checksum: 8,
+	}
+
 	t.Run("Parse normal", func(t *testing.T) {
 		tle := `ISS (ZARYA)
 1 25544U 98067A   20274.51782528  .00000867  00000-0  22813-4 0  9991
 2 25544  51.6441  93.0000 0001400  11.0000 349.0000 15.49300070250768
 	`
 
-		expected := TLE{
-			Name:  "ISS (ZARYA)",
-			Line1: "1 25544U 98067A   20274.51782528  .00000867  00000-0  22813-4 0  9991",
-			Line2: "2 25544  51.6441  93.0000 0001400  11.0000 349.0000 15.49300070250768",
-
-			NoradId:        25544,
-			Classification: "U",
-
-			LaunchTwoDigitYear: "98",
-			LaunchNumber:       "067",
-			LaunchPiece:        "A",
-		}
 		got, err := Parse(tle)
 		assert.Nil(t, err)
 		assert.Equal(t, expected, got)
@@ -43,18 +67,6 @@ ISS (ZARYA)
 
 	`
 
-		expected := TLE{
-			Name:  "ISS (ZARYA)",
-			Line1: "1 25544U 98067A   20274.51782528  .00000867  00000-0  22813-4 0  9991",
-			Line2: "2 25544  51.6441  93.0000 0001400  11.0000 349.0000 15.49300070250768",
-
-			NoradId:        25544,
-			Classification: "U",
-
-			LaunchTwoDigitYear: "98",
-			LaunchNumber:       "067",
-			LaunchPiece:        "A",
-		}
 		got, err := Parse(tle)
 		assert.Nil(t, err)
 		assert.Equal(t, expected, got)
@@ -65,19 +77,11 @@ ISS (ZARYA)
 2 25544  51.6441  93.0000 0001400  11.0000 349.0000 15.49300070250768
 	`
 
-		expected := TLE{
-			Line1: "1 25544U 98067A   20274.51782528  .00000867  00000-0  22813-4 0  9991",
-			Line2: "2 25544  51.6441  93.0000 0001400  11.0000 349.0000 15.49300070250768",
+		localExpected := expected
+		localExpected.Name = ""
 
-			NoradId:        25544,
-			Classification: "U",
-
-			LaunchTwoDigitYear: "98",
-			LaunchNumber:       "067",
-			LaunchPiece:        "A",
-		}
 		got, err := Parse(tle)
 		assert.Nil(t, err)
-		assert.Equal(t, expected, got)
+		assert.Equal(t, localExpected, got)
 	})
 }
