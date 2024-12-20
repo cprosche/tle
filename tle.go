bdcd89900590546ead44c9f682c8c774daf9a441
“Cade <caderosche@gmail.com>
2024-12-20 08:51:11 -0700

    parsed rest of fields

--- tle.go
+++ tle.go
@@ -41,7 +41,8 @@ type TLE struct {
 	MeanAnomalyDegrees     float64
 	MeanMotion             float64
 	EpochRevolutionCount   int
-	Line2Checksum          int
+
+	Line2Checksum int
 }
 
 func Parse(txt string) (TLE, error) {
@@ -76,6 +77,66 @@ func Parse(txt string) (TLE, error) {
 	result.LaunchPiece = strings.TrimSpace(result.Line1[14:17])
 
 	// TODO: finish parsing the rest of the fields
+	result.EpochYear = result.Line1[18:20]
+	result.EpochDay = result.Line1[20:32]
+
+	result.MeanMotionFirstDerivative = result.Line1[34:43]
+	result.MeanMotionSecondDerivative = result.Line1[45:52]
+
+	result.BStar = result.Line1[54:61]
+
+	result.EphemerisType = result.Line1[62:63]
+	result.ElementSetNumber, err = strconv.Atoi(result.Line1[65:68])
+	if err != nil {
+		return TLE{}, err
+	}
+
+	result.Line1Checksum, err = strconv.Atoi(result.Line1[68:69])
+	if err != nil {
+		return TLE{}, err
+	}
+
+	// Parse the rest of the fields from the second line
+	result.InclinationDegrees, err = strconv.ParseFloat(result.Line2[9:16], 64)
+	if err != nil {
+		return TLE{}, err
+	}
+
+	result.RightAscensionDegrees, err = strconv.ParseFloat(result.Line2[18:25], 64)
+	if err != nil {
+		return TLE{}, err
+	}
+
+	// The eccentricity is a bit tricky because it's a number with an implicit decimal point
+	result.Eccentricity, err = strconv.ParseFloat("."+result.Line2[26:33], 64)
+	if err != nil {
+		return TLE{}, err
+	}
+
+	result.PerigeeArgumentDegrees, err = strconv.ParseFloat(result.Line2[35:42], 64)
+	if err != nil {
+		return TLE{}, err
+	}
+
+	result.MeanAnomalyDegrees, err = strconv.ParseFloat(result.Line2[43:51], 64)
+	if err != nil {
+		return TLE{}, err
+	}
+
+	result.MeanMotion, err = strconv.ParseFloat(result.Line2[52:63], 64)
+	if err != nil {
+		return TLE{}, err
+	}
+
+	result.EpochRevolutionCount, err = strconv.Atoi(result.Line2[63:68])
+	if err != nil {
+		return TLE{}, err
+	}
+
+	result.Line2Checksum, err = strconv.Atoi(result.Line2[68:69])
+	if err != nil {
+		return TLE{}, err
+	}
 
 	return result, nil
 }
